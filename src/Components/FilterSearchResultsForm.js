e3e91c0f39e1aa457ab8bd48fcf9f6d9d7d51254
Owner <shayleebaumgartner2002@gmail.com>
2024-07-21 18:47:10 -0400

    making the filter option work with both, or one parts of the search and location search options

--- src/Components/FilterSearchResultsForm.js
+++ src/Components/FilterSearchResultsForm.js
@@ -1,12 +1,68 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './FilterSearchResultsForm.css';
+import Select from 'react-select';
 
-const FilterSearchResultsForm = () => {
-    return (
-        <div className="">
-            FILTERING...
-        </div>
-    )
+const salaryOptions = [
+    { name: 'salary', value: '25000', label: '$25,000' },
+    { name: 'salary', value: '35000', label: '$35,000' },
+    { name: 'salary', value: '45000', label: '$45,000' },
+    { name: 'salary', value: '65000', label: '$65,000' },
+    { name: 'salary', value: '85000', label: '$85,000' },
+    { name: 'salary', value: '100000', label: '$100,000' },
+  ]
+
+const hoursOptions = [
+  { name: 'hours', value: 'full-time', label: 'Full-time' },
+  { name: 'hours', value: 'part-time', label: 'Part-time' },
+]
+
+const sortByOptions = [
+    { name: 'sort_by', value: 'hybrid', label: 'Hybrid' },
+    { name: 'sort_by', value: 'date', label: 'Date' },
+    { name: 'sort_by', value: 'salary', label: 'Salary' },
+    { name: 'sort_by', value: 'relevance', label: 'Relevance' },
+  ]
+
+  const jobTypeOptions = [
+    { name: 'job_type', value: 'permanent', label: 'Permanent' },
+    { name: 'job_type', value: 'contract', label: 'Contract' },
+  ]
+
+const FilterSearchResultsForm = ({grabFilteredFormData}) => {
+    const INITIAL_STATE = {
+        salary: '25000',
+        hours: 'full-time',
+        job_type: 'permanent',
+        sort_by: 'salary'
+    }
+    const  [formData, setFormData] =  useState(INITIAL_STATE);
+
+	const  handleChange = (evt) => {
+        const value = evt.value;
+		setFormData({
+            ...formData,
+            [evt.name]: value
+          });
+	};
+
+    useEffect(() => {
+        const handleSubmit = () => {
+            if(formData !== INITIAL_STATE){
+                grabFilteredFormData(formData);
+            }
+        }
+
+        handleSubmit();
+    }, [formData])
+
+return  (
+  <form className="filterContainer">
+    <Select className="dropDownOptions" placeholder="Salary" options={salaryOptions} onChange={handleChange} />
+    <Select className="dropDownOptions" placeholder="Hours" options={hoursOptions} onChange={handleChange}/>
+    <Select className="dropDownOptions" placeholder="Sort by" options={sortByOptions} onChange={handleChange}/>
+    <Select className="dropDownOptions" placeholder="Job Type" options={jobTypeOptions} onChange={handleChange}/>
+  </form>
+)
 }
 
 export default FilterSearchResultsForm;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,9 +8,16 @@ import "./Home.css";
 const Home = () => {
     const [searchedJobs, setSearchedJobs] = useState(false);
     const [axiosResults, setAxiosResults] = useState(false);
+    const [filteredJobs, setFilteredJobs] = useState(false);
 
     const grabFormData = useCallback((formData) => {
         setSearchedJobs(formData);
+        console.log(formData, 'FORM DAT IN SERAHED JOBS')
+    }, []);
+
+    const grabFilteredFormData = useCallback((filterdFormData) => {
+        console.log(filterdFormData, 'formData IN HOME FILTERED');
+        setFilteredJobs(filterdFormData);
     }, []);
 
     const loadJobListingContainer = () => {
@@ -41,10 +48,33 @@ const Home = () => {
        sendAxiosRequest();
     }, [searchedJobs])
 
+    useEffect(() => {
+        const sendAxiosRequest = async () => {
+            if(filteredJobs !== false){
+                if(searchedJobs !== false){
+                    let results = await axios.get(`http://api.adzuna.com:80/v1/api/jobs/us/search/1?app_id=e8c48744&app_key=317b825c177fbca1dc98cc72803c5352&what=${searchedJobs.title.replaceAll(' ', '%20')}&where=${searchedJobs.location.replaceAll(' ', '%20').replaceAll(',', '%2C')}&sort_by=${filteredJobs.sort_by}&salary_min=${filteredJobs.salary}`);
+                    setAxiosResults(results.data.results);
+                } else if (searchedJobs.title === '') {
+                    let results = await axios.get(`http://api.adzuna.com:80/v1/api/jobs/us/search/1?app_id=e8c48744&app_key=317b825c177fbca1dc98cc72803c5352&where=${searchedJobs.location.replaceAll(' ', '%20').replaceAll(',', '%2C')}sort_by=${filteredJobs.sort_by}&salary_min=${filteredJobs.salary}`);
+                    setAxiosResults(results.data.results);
+                } else if (searchedJobs.location === '') {
+                    let results = await axios.get(`http://api.adzuna.com:80/v1/api/jobs/us/search/1?app_id=e8c48744&app_key=317b825c177fbca1dc98cc72803c5352&what=${searchedJobs.title.replaceAll(' ', '%20')}&sort_by=${filteredJobs.sort_by}&salary_min=${filteredJobs.salary}`);
+                    setAxiosResults(results.data.results);
+                } else {
+                    let results = await axios.get(`http://api.adzuna.com:80/v1/api/jobs/us/search/1?app_id=e8c48744&app_key=317b825c177fbca1dc98cc72803c5352&sort_by=${filteredJobs.sort_by}&salary_min=${filteredJobs.salary}`);
+                    setAxiosResults(results.data.results);
+                }
+                //http://api.adzuna.com:80/v1/api/jobs/gb/search/1?app_id={YOUR_APP_ID}&app_key={YOUR_APP_KEY}&results_per_page=20&what=javascript%20developer&what_exclude=java&where=london&sort_by=salary&salary_min=30000&full_time=1&permanent=1&content-type=application/json
+            }
+        }
+
+       sendAxiosRequest();
+    }, [filteredJobs])
+
     return (
         <div>
             <SearchBar grabFormData={grabFormData}/>
-            <FilterSearchResultsForm />
+            <FilterSearchResultsForm grabFilteredFormData={grabFilteredFormData}/>
             {loadJobListingContainer()}
         </div>
     )
