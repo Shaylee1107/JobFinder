ab02f69cae4c72f463fb652313473dada7649ff0
Owner <shayleebaumgartner2002@gmail.com>
2024-07-22 17:44:54 -0400

    making bookmark toggle on and off

--- src/Components/JobListing.js
+++ src/Components/JobListing.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './JobListings.css';
 
 const JobListings = ({title, company, location, salary, description, companyWebsite}) => {
+    const [isBookMarked, setIsBookMarked] = useState(false);
+
     const redirectToCompanyWebsite = () => {
         window.open(companyWebsite, "_blank", "noreferrer");
     }
 
+    const showBookMark = () => {
+        if(isBookMarked === false){
+            return (
+                <div>
+                  <img 
+                    src="https://static-00.iconduck.com/assets.00/bookmark-icon-1803x2048-3d3yni5w.png" 
+                    className="bookmark-icon" 
+                    onClick={() => setIsBookMarked(mark => !mark)}
+                    alt="bookmark icon" 
+                  />
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                  <img 
+                    src="https://static-00.iconduck.com/assets.00/bookmark-icon-1567x2048-gqmwqm5z.png" 
+                    className="blue-bookmark-icon" 
+                    onClick={() => setIsBookMarked(mark => !mark)}
+                    alt="bookmark icon" 
+                  />
+                </div>
+            )
+        }
+    }
+
     return (
-        <div onClick={() => redirectToCompanyWebsite()} className="JobListContainer">
+        <div className="JobListContainer">
             <h3>{title}</h3>
+            {
+                showBookMark()
+            }
             <p>{company}</p>
             <p>{location}</p>
             <p>${salary}</p>
             <p>{description}</p>
+            <button onClick={() => redirectToCompanyWebsite()}>Apply Now</button>
         </div>
     )
 }
