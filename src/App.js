37f0dc1feca8cb3fae083b481d3663acafeadaca
Owner <shayleebaumgartner2002@gmail.com>
2024-07-27 19:01:03 -0400

    styling navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import AppRoutes from './Routes/AppRoutes';
 import Navbar from './Routes/Navbar';
 
--- src/Routes/Navbar.js
+++ src/Routes/Navbar.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./Navbar.css";
 
 const Navbar = () => {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/favorites">Favorites</Link>
-        </nav>
+        <div className='flex'>
+            <div>
+              <img 
+                className='img nabar-items' 
+                src="https://cdn1.iconfinder.com/data/icons/flat-design-basic-set-7/24/location-map-where-global-positioning-system-512.png" 
+                alt="location icon"
+              />
+              <h1 className='title nabar-items montserrat'>Job Finder</h1>
+            
+                <Link className='nabar-items links montserrat' to="/">Home</Link>
+                <Link className='nabar-items links montserrat' to="/favorites">Favorites</Link>
+                </div>
+        </div>
     )
 }
 
