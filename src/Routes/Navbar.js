2b2383f889e7bb9c36db08cc5954bc84a29d346d
Owner <shayleebaumgartner2002@gmail.com>
2024-07-31 18:30:55 -0400

    remove the Home button and makeing Job Finder the home, also writing tests for Navbar

--- src/Routes/Navbar.js
+++ src/Routes/Navbar.js
@@ -11,11 +11,9 @@ const Navbar = () => {
                 src="https://cdn1.iconfinder.com/data/icons/flat-design-basic-set-7/24/location-map-where-global-positioning-system-512.png" 
                 alt="location icon"
               />
-              <h1 className='title nabar-items montserrat'>Job Finder</h1>
-            
-                <Link className='nabar-items links montserrat' to="/">Home</Link>
-                <Link className='nabar-items links montserrat' to="/favorites">Favorites</Link>
-                </div>
+              <Link className='title montserrat' to="/" >Job Finder</Link>
+              <Link className='nabar-items links montserrat' to="/favorites">Favorites</Link>
+              </div>
         </div>
     )
 }
---
+++ src/Tests/Navbar.test.js
@@ -0,0 +1,54 @@
+import {render, fireEvent, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import Navbar from '../Routes/Navbar';
+import { MemoryRouter } from 'react-router-dom';
+
+test('if navbar matches snapshot', () => {
+  const {asFragment, getByText} = render(<MemoryRouter><Navbar /></MemoryRouter>);
+  expect(getByText('Job Finder')).toBeInTheDocument();
+  expect(getByText('Favorites')).toBeInTheDocument();
+  expect(asFragment()).toMatchInlineSnapshot(`
+   <DocumentFragment>
+     <div
+       class="flex"
+     >
+       <div>
+         <img
+           alt="location icon"
+           class="img nabar-items"
+           src="https://cdn1.iconfinder.com/data/icons/flat-design-basic-set-7/24/location-map-where-global-positioning-system-512.png"
+         />
+         <a
+           class="title montserrat"
+           href="/"
+         >
+           Job Finder
+         </a>
+         <a
+           class="nabar-items links montserrat"
+           href="/favorites"
+         >
+           Favorites
+         </a>
+       </div>
+     </div>
+   </DocumentFragment>
+  `)
+});
+
+test('should navigate to ... when link is clicked', () => {
+  const { getByText } = render(<MemoryRouter><Navbar /></MemoryRouter>);
+
+  const favoritesLink = getByText('Favorites');
+  const jobFinderLink = getByText('Job Finder');
+
+  fireEvent.click(favoritesLink);
+  fireEvent.click(jobFinderLink); 
+
+  expect(screen.getByText('Favorites').closest('a')).toHaveAttribute('href', '/favorites');
+  expect(screen.getByText('Job Finder').closest('a')).toHaveAttribute('href', '/');
+
+});
+
+
