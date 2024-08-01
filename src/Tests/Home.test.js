4189397dc82ff03eb96da8b6a2cafec2b3aee922
Owner <shayleebaumgartner2002@gmail.com>
2024-08-01 19:59:52 -0400

    adding Home Page testing

--- src/Tests/Home.test.js
+++ src/Tests/Home.test.js
@@ -1,12 +1,31 @@
-import {render, fireEvent, screen} from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Home from '../Pages/Home';
+import Home from '../Pages/Home.js';
+import LoadingProvider from '../Providers/LoadingProvider.js';
+import { waitFor } from '@testing-library/react';
+import FavoritesProvider from '../Providers/FavoritesProvider.js';
 
-test('if Home renders Search Bar and Filter Search Options', () => {
-    const {asFragment, getByText} = render(<Home />);
+window.scrollTo = jest.fn();
 
-    expect(getByText('Job Title:')).toBeInTheDocument();
-    expect(getByText('Location')).toBeInTheDocument();
-    expect(getByText('Search')).toBeInTheDocument();
+test('if Home renders Search Bar and Filter Search Options', async () => {
+    const { getByText } = render( 
+    <LoadingProvider>
+        <FavoritesProvider>
+         <Home />
+        </FavoritesProvider>
+    </LoadingProvider>
+    );
+
+    await waitFor(() => expect(getByText('Job Title:')).toBeInTheDocument());
+    await waitFor(() => expect(getByText('Location:')).toBeInTheDocument());
+    await waitFor(() => expect(getByText('Search')).toBeInTheDocument());
+    await waitFor(() => expect(getByText('Salary')).toBeInTheDocument());
+    await waitFor(() => expect(getByText('Hours')).toBeInTheDocument());
+    await waitFor(() => expect(getByText('Sort by')).toBeInTheDocument());
+    await waitFor(() => expect(getByText('Job Type')).toBeInTheDocument());
    
 });
+
+
+
