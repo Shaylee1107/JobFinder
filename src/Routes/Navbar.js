b11d2951fa7050fad37e1ca160baf852dfa7b02d
Owner <shayleebaumgartner2002@gmail.com>
2024-08-06 16:31:50 -0400

    making icon go back to the homepage

--- src/Routes/Navbar.js
+++ src/Routes/Navbar.js
@@ -6,11 +6,12 @@ const Navbar = () => {
     return (
         <div className='flex'>
             <div>
-              <img 
+              <Link to="/"><img 
                 className='img nabar-items' 
                 src="https://cdn1.iconfinder.com/data/icons/flat-design-basic-set-7/24/location-map-where-global-positioning-system-512.png" 
                 alt="location icon"
               />
+              </Link>
               <Link className='title montserrat' to="/" >Job Finder</Link>
               <Link className='nabar-items links montserrat' to="/favorites">Favorites</Link>
               </div>
