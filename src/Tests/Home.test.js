e079e6417bbf65710ce52e0e4b5a7f4265b8d187
Owner <shayleebaumgartner2002@gmail.com>
2024-08-06 11:41:05 -0400

    making mock api axios default responce

--- src/Tests/Home.test.js
+++ src/Tests/Home.test.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { render, cleanup, } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Home from '../Pages/Home.js';
 import LoadingProvider from '../Providers/LoadingProvider.js';
 import { waitFor } from '@testing-library/react';
 import FavoritesProvider from '../Providers/FavoritesProvider.js';
+import axios from 'axios';
 
-afterEach(cleanup);
+window.scrollTo = jest.fn();
+// jest.mock('axios');
+// const expectedResponse = JSON.stringify({
+//   "data" : {
+//       "results": {
+//               "id": "4806158050",
+//               "redirect_url": "https://www.adzuna.com/land/ad/4804048782?se=BEeXQfNR7xGYKGb7g6DTQA&utm_medium=api&utm_source=a7ebf48d&v=0E88B0A5E036592D574963265D6DCC53F9C26EE7",
+//               "title": "Software Engineering Manager",
+//               "description": "Requisition ID: R10133765 Category: Engineering Location: Melbourne, Florida, United States of America Clearance Type: Secret Telecommute: No- Teleworking not available for this position Shift: 1st Shift (United States of America) Travel Required: Yes, 10% of the Time Relocation Assistance: Relocation assistance may be available Positions Available: 20 At Northrop Grumman, our employees have incredible opportunities to work on revolutionary systems that impact people's lives around the world to...",
+//               "company": {
+//                   "display_name": "Northrop Grumman"
+//                 },
+//               "location": {
+//                 "area": [
+//                   "US",
+//                   "California",
+//                   "Ventura County",
+//                   "Santa Rosa Valley"
+//                  ]
+//                },
+//                "salary_max": 124119.98,
+//           }
+//   }
+// });
+
+// jest.mock('axios', () => ({
+//   get: jest.fn(() => Promise.resolve({ data: { results: [] } })),
+// }));
 
 jest.mock("axios", () => {
     const expectedResponse = JSON.stringify({
@@ -34,7 +62,23 @@ jest.mock("axios", () => {
     return () => new Promise(() => expectedResponse);
   })
 
-window.scrollTo = jest.fn();
+// describe('MyComponent', () => {
+//   it('should fetch data and render correctly', async () => {
+//     const { getByText } = render( 
+//       <LoadingProvider>
+//           <FavoritesProvider>
+//            <Home />
+//           </FavoritesProvider>
+//       </LoadingProvider>
+//       );
+
+//     expect(axios.get).toHaveBeenCalledWith(
+//       'https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=a7ebf48d&app_key=a31ecdf957770c324646f06209fa554c'
+//     );
+
+   
+//   });
+// });
 
 test('if Home renders Search Bar and Filter Search Options', async () => {
     const { getByText } = render( 
@@ -55,21 +99,17 @@ test('if Home renders Search Bar and Filter Search Options', async () => {
    
 });
 
-test('if an API request goes through after clicking search', async () => {
-    const { getAllByTestId } = render( 
+
+test('if home has jobs rendered upon opening the website', async () => {
+    const { getByText } = render( 
     <LoadingProvider>
         <FavoritesProvider>
          <Home />
         </FavoritesProvider>
     </LoadingProvider>
     );
-
-
-
-    // const firstRender = asFragment();
-
-    // fireEvent.click(getByText("Search"));
-
+   
+    // expect(axios.get).toHaveBeenCalledTimes(1);
     // await waitFor(() => expect(getByText('Software Engineering Manager')).toBeInTheDocument());
    
 });
@@ -78,7 +118,156 @@ test('if an API request goes through after clicking search', async () => {
 
 
 
+// test('if home has jobs rendered upon opening the website', async () => {
+//     const { getByText } = render( 
+//     <LoadingProvider>
+//         <FavoritesProvider>
+//          <Home />
+//         </FavoritesProvider>
+//     </LoadingProvider>
+//     );
+//     const job = {
+//       "results": {
+//         "id": "4806158050",
+//         "redirect_url": "https://www.adzuna.com/land/ad/4804048782?se=BEeXQfNR7xGYKGb7g6DTQA&utm_medium=api&utm_source=a7ebf48d&v=0E88B0A5E036592D574963265D6DCC53F9C26EE7",
+//         "title": "Software Engineering Manager",
+//         "description": "Requisition ID: R10133765 Category: Engineering Location: Melbourne, Florida, United States of America Clearance Type: Secret Telecommute: No- Teleworking not available for this position Shift: 1st Shift (United States of America) Travel Required: Yes, 10% of the Time Relocation Assistance: Relocation assistance may be available Positions Available: 20 At Northrop Grumman, our employees have incredible opportunities to work on revolutionary systems that impact people's lives around the world to...",
+//         "company": {
+//             "display_name": "Northrop Grumman"
+//           },
+//         "location": {
+//           "area": [
+//             "US",
+//             "California",
+//             "Ventura County",
+//             "Santa Rosa Valley"
+//            ]
+//          },
+//          "salary_max": 124119.98,
+//     }
+//     };
+//     const responce = {data: job};
+
+//     await axios.get.mockResolvedValue({responce});
+//     await waitFor(() => expect(getByText('Software Engineering Manager')).toBeInTheDocument());
+   
+// });
+
+
+
+// jest.spyOn(axios, 'get').mockResolvedValueOnce({
+//   data: [
+//     {
+//       userId: 1,
+//       id: 1,
+//       title: 'test'
+//     }
+//   ]
+// });
+
+// test('fetches data on button click', async () => {
+//   const { getByText } = render(<MyComponent />);
+//   const button = getByText('Fetch Data');
+
+//   fireEvent.click(button);
+
+//   // Assert that the API call was made (you can mock axios here too)
+//   // Assert other component behavior as needed
+// });
 
 
 
 
+
+// jest.mock('axios', () => ({
+//   get: jest.fn(() => Promise.resolve({ data: 'data' })),
+// }));
+
+// jest.mock("axios", () => {
+//     const expectedResponse = JSON.stringify({
+//         "data" : {
+//             "results": {
+//                     "id": "4806158050",
+//                     "redirect_url": "https://www.adzuna.com/land/ad/4804048782?se=BEeXQfNR7xGYKGb7g6DTQA&utm_medium=api&utm_source=a7ebf48d&v=0E88B0A5E036592D574963265D6DCC53F9C26EE7",
+//                     "title": "Software Engineering Manager",
+//                     "description": "Requisition ID: R10133765 Category: Engineering Location: Melbourne, Florida, United States of America Clearance Type: Secret Telecommute: No- Teleworking not available for this position Shift: 1st Shift (United States of America) Travel Required: Yes, 10% of the Time Relocation Assistance: Relocation assistance may be available Positions Available: 20 At Northrop Grumman, our employees have incredible opportunities to work on revolutionary systems that impact people's lives around the world to...",
+//                     "company": {
+//                         "display_name": "Northrop Grumman"
+//                       },
+//                     "location": {
+//                       "area": [
+//                         "US",
+//                         "California",
+//                         "Ventura County",
+//                         "Santa Rosa Valley"
+//                        ]
+//                      },
+//                      "salary_max": 124119.98,
+//                 }
+//         }
+//     });
+//     return () => new Promise(() => expectedResponse);
+//   })
+
+  // jest.mock("axios", () => {
+  //   get: jest.fn((jobTitle) => {
+  //     if(jobTitle === 'Software Engineering Manager'){
+  //         return Promise.resolve({
+  //             data : {
+  //                 "results": {
+  //                         "id": "4806158050",
+  //                         "redirect_url": "https://www.adzuna.com/land/ad/4804048782?se=BEeXQfNR7xGYKGb7g6DTQA&utm_medium=api&utm_source=a7ebf48d&v=0E88B0A5E036592D574963265D6DCC53F9C26EE7",
+  //                         "title": "Software Engineering Manager",
+  //                         "description": "Requisition ID: R10133765 Category: Engineering Location: Melbourne, Florida, United States of America Clearance Type: Secret Telecommute: No- Teleworking not available for this position Shift: 1st Shift (United States of America) Travel Required: Yes, 10% of the Time Relocation Assistance: Relocation assistance may be available Positions Available: 20 At Northrop Grumman, our employees have incredible opportunities to work on revolutionary systems that impact people's lives around the world to...",
+  //                         "company": {
+  //                             "display_name": "Northrop Grumman"
+  //                           },
+  //                         "location": {
+  //                           "area": [
+  //                             "US",
+  //                             "California",
+  //                             "Ventura County",
+  //                             "Santa Rosa Valley"
+  //                            ]
+  //                          },
+  //                          "salary_max": 124119.98,
+  //                     }
+  //             }
+  //         });
+  //     }
+  // })
+  
+  // })
+
+
+
+
+
+      // const searchButton = getByText("Search");
+
+      // fireEvent.click(searchButton);
+
+      // axios.get = jest.fn().mockResolvedValue({
+      //   data : {
+      //     "results": {
+      //             "id": "4806158050",
+      //             "redirect_url": "https://www.adzuna.com/land/ad/4804048782?se=BEeXQfNR7xGYKGb7g6DTQA&utm_medium=api&utm_source=a7ebf48d&v=0E88B0A5E036592D574963265D6DCC53F9C26EE7",
+      //             "title": "Software Engineering Manager",
+      //             "description": "Requisition ID: R10133765 Category: Engineering Location: Melbourne, Florida, United States of America Clearance Type: Secret Telecommute: No- Teleworking not available for this position Shift: 1st Shift (United States of America) Travel Required: Yes, 10% of the Time Relocation Assistance: Relocation assistance may be available Positions Available: 20 At Northrop Grumman, our employees have incredible opportunities to work on revolutionary systems that impact people's lives around the world to...",
+      //             "company": {
+      //                 "display_name": "Northrop Grumman"
+      //               },
+      //             "location": {
+      //               "area": [
+      //                 "US",
+      //                 "California",
+      //                 "Ventura County",
+      //                 "Santa Rosa Valley"
+      //                ]
+      //              },
+      //              "salary_max": 124119.98,
+      //         }
+      // }
+      // });
+      
+      // expect(axios.get).toHaveBeenCalledTimes(1);
