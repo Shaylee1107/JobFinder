50b5b10e274e45a2eefc3ebef37e18cb9022ef77
Owner <shayleebaumgartner2002@gmail.com>
2024-08-07 17:39:16 -0400

    making test files

--- src/Tests/Navbar.test.js
+++ src/Tests/Navbar.test.js
@@ -4,36 +4,9 @@ import Navbar from '../Routes/Navbar';
 import { MemoryRouter } from 'react-router-dom';
 
 test('if navbar matches snapshot', () => {
-  const {asFragment, getByText} = render(<MemoryRouter><Navbar /></MemoryRouter>);
+  const { getByText } = render(<MemoryRouter><Navbar /></MemoryRouter>);
   expect(getByText('Job Finder')).toBeInTheDocument();
   expect(getByText('Favorites')).toBeInTheDocument();
-  expect(asFragment()).toMatchInlineSnapshot(`
-   <DocumentFragment>
-     <div
-       class="flex"
-     >
-       <div>
-         <img
-           alt="location icon"
-           class="img nabar-items"
-           src="https://cdn1.iconfinder.com/data/icons/flat-design-basic-set-7/24/location-map-where-global-positioning-system-512.png"
-         />
-         <a
-           class="title montserrat"
-           href="/"
-         >
-           Job Finder
-         </a>
-         <a
-           class="nabar-items links montserrat"
-           href="/favorites"
-         >
-           Favorites
-         </a>
-       </div>
-     </div>
-   </DocumentFragment>
-  `)
 });
 
 test('should navigate to ... when link is clicked', () => {
