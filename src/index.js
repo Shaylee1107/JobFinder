8f68042706a163897111aea562e47874ded36777
Owner <shayleebaumgartner2002@gmail.com>
2024-08-08 14:59:04 -0400

    adding msw and changing site url name in package.json to be updated repository name

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import {HashRouter} from "react-router-dom";
 import FavoritesProvider from './Providers/FavoritesProvider';
 import LoadingProvider from './Providers/LoadingProvider';
+import { server } from './mocks/browser'
+ 
+server.listen()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,6 +22,22 @@ root.render(
     </LoadingProvider>
   </React.StrictMode>
 );
+
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return
+  }
+ 
+  const { worker } = await import('./mocks/browser')
+ 
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start()
+}
+ 
+enableMocking().then(() => {
+  ReactDOM.render(<App />, rootElement)
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/mocks/handlers.js
@@ -0,0 +1,30 @@
+import { rest } from 'msw';
+
+export const handlers = [
+    rest.get('https://example.com/user', () => {
+      return HttpResponse.json({
+        data : {
+          results: [
+              {
+                  "id": "4806158050",
+                  "redirect_url": "https://www.adzuna.com/land/ad/4804048782?se=BEeXQfNR7xGYKGb7g6DTQA&utm_medium=api&utm_source=a7ebf48d&v=0E88B0A5E036592D574963265D6DCC53F9C26EE7",
+                  "title": "Software Engineering Manager",
+                  "description": "Requisition ID: R10133765 Category: Engineering Location: Melbourne, Florida, United States of America Clearance Type: Secret Telecommute: No- Teleworking not available for this position Shift: 1st Shift (United States of America) Travel Required: Yes, 10% of the Time Relocation Assistance: Relocation assistance may be available Positions Available: 20 At Northrop Grumman, our employees have incredible opportunities to work on revolutionary systems that impact people's lives around the world to...",
+                  "company": {
+                      "display_name": "Northrop Grumman"
+                    },
+                  "location": {
+                    "area": [
+                      "US",
+                      "California",
+                      "Ventura County",
+                      "Santa Rosa Valley"
+                     ]
+                   },
+                   "salary_max": 124119.98,
+              }
+            ]
+      }
+      })
+    }),
+  ]
