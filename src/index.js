d15fda849658c66fa6dd1b1fac942713c676b706
Owner <shayleebaumgartner2002@gmail.com>
2024-08-13 13:56:54 -0400

    trying to get msw to work

--- src/index.js
+++ src/index.js
@@ -6,38 +6,37 @@ import reportWebVitals from './reportWebVitals';
 import {HashRouter} from "react-router-dom";
 import FavoritesProvider from './Providers/FavoritesProvider';
 import LoadingProvider from './Providers/LoadingProvider';
-// import { server } from './mocks/browser'
- 
-// server.listen()
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <LoadingProvider>
-    <FavoritesProvider>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </FavoritesProvider>
-    </LoadingProvider>
-  </React.StrictMode>
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return
+  }
+ 
+  const { worker } = await import('./mocks/browser')
+ 
+  return worker.start()
+}
+ 
+enableMocking().then(() => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <mockServiceWorker>
+      <LoadingProvider>
+      <FavoritesProvider>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </FavoritesProvider>
+      </LoadingProvider>
+      </mockServiceWorker>
+    </React.StrictMode>
+  );
+})
 
-// async function enableMocking() {
-//   if (process.env.NODE_ENV !== 'development') {
-//     return
-//   }
- 
-//   const { worker } = await import('./mocks/browser')
- 
-//   // `worker.start()` returns a Promise that resolves
-//   // once the Service Worker is up and ready to intercept requests.
-//   return worker.start()
-// }
- 
-// enableMocking().then(() => {
-//   ReactDOM.render(<App />, rootElement)
-// })
+  
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
